9121db2e53e050c270a7f8d0e71b7550c7d73aff
Punky <rahiguera91@gmail.com>
2024-01-12 08:34:30 -0500

    adicionado lista de healers

--- components/characters-list.jsx
+++ components/characters-list.jsx
@@ -46,7 +46,7 @@ export const CharactersList = ({ items }) => {
                     </Card>
                     <Card shadow="sm" isPressable className="col-span-4 md:col-span-11 ms-0 mb-2">
                         <CardBody className="overflow-visible p-0">
-                            <div className={'absolute col-span-6 justify-self-start bg-gray-800 bg-opacity-30 h-full z-10'}
+                            <div className={'absolute col-span-6 justify-self-start bg-gray-800 bg-opacity-40 h-full z-10 rounded-r-2xl'}
                                  style={{ width: `${item.v}` }}></div>
                             <div
                                 className={`flex grid-cols-12 gap-6 md:gap-4 ${classColor[item.c].bg} bg-opacity-25 h-full items-center justify-between`}>
