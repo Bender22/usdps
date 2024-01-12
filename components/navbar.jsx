79e06191ef5d40b4879a9f6980ffc8b10f9e63b4
Punky <rahiguera91@gmail.com>
2024-01-12 12:50:35 -0500

    adicionado lista de healers

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -125,10 +125,10 @@ export const Navbar = () => {
 
 					<ThemeSwitch />
 				</NavbarItem>
-				<NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
-				<NavbarItem className="hidden md:flex">
+				{/* <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem> */}
+				{/* <NavbarItem className="hidden md:flex"> */}
 
-				</NavbarItem>
+				{/* </NavbarItem> */}
 			</NavbarContent>
 
 			{/* <NavbarContent className="sm:hidden basis-1 pl-4" justify="end"> */}
